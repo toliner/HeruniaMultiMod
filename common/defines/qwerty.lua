9f70561c7ca8aaacecbad84eb8ee68952188cf73
toliner <toliner@toliner.dev>
2022-03-27 14:30:43 +0900

    change defines

--- common/defines/qwerty.lua
+++ common/defines/qwerty.lua
@@ -13,8 +13,18 @@ NDefines.NDiplomacy.ASSUME_FACTION_LEADERSHIP_MIN_MANPOWER_RATIO = 1;
 NDefines.NDiplomacy.ASSUME_FACTION_LEADERSHIP_MIN_FACTORY_RATIO = 1;
 NDefines.NDiplomacy.ASSUME_FACTION_LEADERSHIP_COOLDOWN_DAYS = 90;
 
--- 燃料改善: 全部倍
--- NDefines.NCountry.STARTING_FUEL_RATIO = 0.5
--- NDefines.NCountry.BASE_FUEL_GAIN = 4.0
--- NDefines.NCountry.BASE_FUEL_GAIN_PER_OIL = 4.0
--- NDefines.NCountry.BASE_FUEL_CAPACITY = 100000
+-- 経験値周り調整
+-- 経験値と指揮力の最大値を上げる
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 400;
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 99999;
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 99999;
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 99999;
+-- 師団編成コスト下げる
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 1;
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 1;
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 1;
+-- 設計コスト下げる
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 1.0;           --5.0
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 1.0;       --6.0
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 1.0;       --3.0
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 1.0;        --1.0
